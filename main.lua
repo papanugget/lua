c4ffc413d22115f106a487641662930c065bfd6b
Albert Chan <evilgenius04@gmail.com>
2025-01-20 21:37:44 -0500

    added anim8 and playsprite sheet

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 function love.load()
+    anim8 = require 'libraries/anim8/anim8'
     player = {}
     player.x = 400
     player.y = 200
     player.speed = 5
     player.sprite = love.graphics.newImage('assets/sprites/parrot.png')
+    player.spriteSheet = love.graphics.newImage('assets/sprites/player-sheet.png')
+    player.grid = anim8.newGrid( 12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight() )
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation( player.grid('1-4', 1), 0.3 )
+
+
     background = love.graphics.newImage('assets/sprites/background.png')
 end
 
@@ -23,10 +31,13 @@ function love.update(dt)
     if love.keyboard.isDown("up") then
         player.y = player.y - player.speed
     end
+
+    player.animations.down:update(dt)
 end
 
 function love.draw()
     -- love.graphics.circle("fill", player.x, player.y, 100)
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(player.sprite, player.x, player.y)
+    --love.graphics.draw(player.sprite, player.x, player.y)
+    player.animations.down:draw(player.spriteSheet, player.x, player.y)
 end
